18f271ff74ed6e0255a7a88cbcea6c3bb11f57fb
wturnips <wturnips@gmail.com>
2022-01-08 14:45:27 -0600

    use conditions and add player varaibles

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,6 +21,14 @@ var fight = function() {
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
 
+  // Check enemys health
+  if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+  }
+  else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
   playerHealth = playerHealth - enemyAttack;
 
@@ -28,6 +36,13 @@ var fight = function() {
   console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
+
+  if (playerHealth <= 0) {
+      window.alert(playerName + " has died!");
+  }
+  else {
+      window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
 };
 
 fight();
